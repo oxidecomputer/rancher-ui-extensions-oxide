373133b8a3203eb86660d18967ee217b5703c53d
Matthew Sanabria <matthew.sanabria@oxide.computer>
2025-03-04 14:02:42 -0500

    oxide: load icon from file

--- pkg/oxide/index.ts
+++ pkg/oxide/index.ts
@@ -7,5 +7,5 @@ export default function(plugin: IPlugin): void {
   plugin.metadata = require('./package.json');
 
   // This specifically sets the icon for the node driver, not the extension itself.
-  plugin.register('image', 'providers/oxide.svg', 'https://oxide.computer/img/press/wordmark-black.svg');
+  plugin.register('image', 'providers/oxide.svg', require('./oxide.svg'));
 }
