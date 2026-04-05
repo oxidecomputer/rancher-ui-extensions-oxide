22bb77ee9d6281c2781a71ec04a82c3cd5c44011
Matthew Sanabria <matthew.sanabria@oxide.computer>
2026-04-04 23:23:02 -0400

    feat: cloud-credential component (#29)
    Updated the application and extension scaffolding and added the
    `cloud-credential` component in the `oxide` extension.
    
    Closes SSE-164.
    
    Amp-Thread:
    https://ampcode.com/threads/T-019cdb3e-bf59-70e8-a1e4-b7ac88f9fa7a
    Amp-Thread:
    https://ampcode.com/threads/T-019d5b77-f857-702c-bfcf-b2487d675766

---
+++ pkg/oxide/cloud-credential/oxide.vue
@@ -0,0 +1,99 @@
+<script>
+import CreateEditView from '@shell/mixins/create-edit-view';
+import { LabeledInput } from '@components/Form/LabeledInput';
+import FormValidation from '@shell/mixins/form-validation';
+
+export default {
+  emits: ['validationChanged'],
+
+  components: { LabeledInput },
+  mixins:     [CreateEditView, FormValidation],
+
+  data() {
+    return {
+      fvFormRuleSets: [
+        { path: 'decodedData.host', rules: ['required'] },
+        { path: 'decodedData.token', rules: ['required'] },
+      ]
+    };
+  },
+
+  created() {
+    this.$emit('validationChanged', false);
+  },
+
+  watch: {
+    fvFormIsValid(newValue) {
+      this.$emit('validationChanged', !!newValue);
+    }
+  },
+
+  methods: {
+    /**
+     * Parse the user-provided host value into a hostname suitable
+     * for the Rancher proxy path. Strips protocol and trailing
+     * slashes.
+     */
+    proxyHost(host) {
+      let h = (host || '').trim().replace(/\/+$/, '');
+
+      if (h.startsWith('https://')) {
+        h = h.slice('https://'.length);
+      } else if (h.startsWith('http://')) {
+        h = h.slice('http://'.length);
+      }
+
+      return h;
+    },
+
+    async test() {
+      try {
+        const host = this.proxyHost(
+          this.value.decodedData.host
+        );
+        const token = this.value.decodedData.token;
+
+        await this.$store.dispatch('management/request', {
+          url:                  `/meta/proxy/${ host }/v1/ping`,
+          headers:              {
+            Accept:               'application/json',
+            'x-api-auth-header':  `Bearer ${ token }`,
+          },
+          redirectUnauthorized: false,
+        });
+
+        return true;
+      } catch (e) {
+        return false;
+      }
+    }
+  }
+};
+</script>
+
+<template>
+  <div>
+    <LabeledInput
+      :value="value.decodedData.host"
+      label-key="cluster.cloudCredential.oxide.host.label"
+      placeholder-key="cluster.cloudCredential.oxide.host.placeholder"
+      :mode="mode"
+      :required="true"
+      :rules="fvGetAndReportPathRules('decodedData.host')"
+      @update:value="value.setData('host', $event)"
+    />
+
+    <div class="mt-20">
+      <LabeledInput
+        :value="value.decodedData.token"
+        label-key="cluster.cloudCredential.oxide.token.label"
+        placeholder-key="cluster.cloudCredential.oxide.token.placeholder"
+        type="password"
+        :mode="mode"
+        :required="true"
+        :rules="fvGetAndReportPathRules('decodedData.token')"
+        @update:value="value.setData('token', $event)"
+      />
+    </div>
+  </div>
+</template>
